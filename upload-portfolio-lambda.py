1c4332bb65795ef2b70346fa8a1e45fe69c6ddaa
Jutzi Christian <Christian.Jutzi@swisscom.com>
2018-08-23 08:25:50 +0200

    python program

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,24 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+
+
+session = boto3.session.Session(profile_name='globi')
+s3 = session.resource('s3')
+
+
+
+myoriginbucket = s3.Bucket('jut-virginia-serverless')
+mytargetbucket = s3.Bucket('jut-virginia')
+mymemory = StringIO.StringIO()
+
+myoriginbucket.download_fileobj('portfoliobuild.zip', mymemory)
+
+with zipfile.ZipFile(mymemory) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        mytargetbucket.upload_fileobj(obj, nm,
+        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        mytargetbucket.Object(nm).Acl().put(ACL='public-read')
